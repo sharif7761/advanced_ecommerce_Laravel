396f3d201255f16ebf948e257ec9595b4ac9a4ad
sharif7761 <sharif7761@gmail.com>
2021-04-22 02:30:54 +0600

    updated subcategory dropdown

--- resources/views/admin/subcategory/edit_subcategory.blade.php
+++ resources/views/admin/subcategory/edit_subcategory.blade.php
@@ -31,9 +31,8 @@
                             <div class="form-group">
                                 <label for="category_id">Category Name</label>
                                 <select name="category_id" class="form-control">
-                                    <option value="{{ $subcategory->category->id }}" selected hidden>{{ $subcategory->category->category_name }}</option>
                                     @foreach($categories as $category)
-                                        <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                                        <option @if($category->id == $subcategory->category->id) echo selected @endif value="{{ $category->id }}">{{ $category->category_name }}</option>
                                     @endforeach
                                 </select>
                             </div>
