35e5dfb077437148eff32ff07f5260ca27d2abd8
sharif7761 <sharif7761@gmail.com>
2021-05-13 20:56:22 +0600

    show featured, trend and best rated products on homepage

--- app/Http/Controllers/FrontPageController.php
+++ app/Http/Controllers/FrontPageController.php
@@ -11,6 +11,9 @@ class FrontPageController extends Controller
     public function index(){
         $categories = Category::all();
         $banner = Product::where('main_slider', true)->latest()->first();
-        return view('pages.index',  compact('categories', 'banner'));
+        $featured = Product::where('status', true)->latest()->take(8)->get();
+        $trend = Product::where([['status', true],['trend', true]])->latest()->take(8)->get();
+        $best_rated = Product::where([['status', true],['best_rated', true]])->latest()->take(8)->get();
+        return view('pages.index',  compact('categories', 'banner', 'featured', 'trend', 'best_rated'));
     }
 }
